52856a7b9f4136c4264a03a5f8ed061fde2a8ef2
Zavorotyuk <v.zavorotyuk@gmail.com>
2018-01-17 23:59:22 +0200

    Add bootstrap, generate core and shared modules, add app.router file

---
+++ src/app/app.router.ts
@@ -0,0 +1,8 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const router: Routes = [
+
+];
+
+export const appRouter: ModuleWithProviders = RouterModule.forRoot(router);
