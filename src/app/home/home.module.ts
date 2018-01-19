3bf625d014dc9d65af1743338a1dd9d98bc240d1
Zavorotyuk <v.zavorotyuk@gmail.com>
2018-01-19 23:19:53 +0200

    Generate tweets and friends component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { homeRouter } from './home.router';
 import { SharedModule } from '../shared/shared.module';
+import { TweetsComponent } from './tweets/tweets.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   imports: [
     SharedModule,
     homeRouter
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, TweetsComponent, FriendsComponent]
 })
 export class HomeModule { }
